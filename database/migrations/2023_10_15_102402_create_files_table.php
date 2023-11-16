b993864b218083a5866f91335e06b5bda3ecb4e5
riskiilyas <riskiilyas03@gmail.com>
2023-11-17 06:57:11 +0700

    fix: files migration

--- database/migrations/2023_10_15_102402_create_files_table.php
+++ database/migrations/2023_10_15_102402_create_files_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('filename');
             $table->string('file_extension');
-            $table->integer('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('enc_type');
             $table->text('iv_encryption');
             $table->text('hashed_key');
